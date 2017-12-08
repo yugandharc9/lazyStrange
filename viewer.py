79bf5b6605d5780244b1ec5c94baf39e6358efc6
yugandharc9@gmail.com <yugandharc9@gmail.com>
2017-12-08 17:01:10 +0530

    Viewer Windows Compatible

--- viewer.py
+++ viewer.py
@@ -1,9 +1,9 @@
 from gui3 import ErrorScreen,DampleApp
 from config import DOCPATH
 import commands,os
+from exceptions import OSError
 
-
-
+"""
 def showfunc(data):
  ls=commands.getoutput('cd ~/docs/%s/ && ls'%(data))
  if 'can\'t' in ls:
@@ -13,8 +13,25 @@ def showfunc(data):
   ls1=ls.split('\n')
   for i in ls1:
    os.system('eog %s'%(DOCPATH+'/'+data+'/'+i))
+"""
 
- 
+def showfunc(data):
+ #ls=commands.getoutput('cd ~/docs/%s/ && ls'%(data))
+ #commands.getoutput('cd %s && ls' %(PATH)).split('\n')
+ try:
+  ls=os.listdir(DOCPATH+data)
+
+ #if 'can\'t' in ls:
+  #d=ErrorScreen()
+  #d.mainloop()
+ #else:
+  #ls1=ls.split('\n')
+  for i in ls:
+   os.system('eog %s'%(DOCPATH+'/'+data+'/'+i))
+ except OSError:
+   d=ErrorScreen()
+   d.mainloop()
+
 w=DampleApp()
 w.mainloop()
 showfunc(w.phone)
